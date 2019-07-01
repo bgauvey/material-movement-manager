f18cf6762b6fdd62ab36698da81870d2f9413583
Bill Gauvey <bgauvey@gmail.com>
2019-07-01 06:17:13 -0400

    removed implemented stuff

--- ClientApp/src/app/models/user.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface IUser {
-  userName: string;
-  hostName: string;
-  user_id: string;
-  user_desc: string;
-}
--- ClientApp/src/app/models/yard-location.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface IYardLocation {
-  row_id: number;
-  item_id: string;
-  location_text: string;
-  last_edit_comment: string;
-  last_edit_by: string;
-  last_edit_at: Date;
-}
